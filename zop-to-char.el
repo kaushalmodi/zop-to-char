2ed3252ff6191a8aebee3d6daea07283ab4000be
Kaushal Modi <kaushal.modi@gmail.com>
2015-01-27 12:22:36 -0500

    Synced to version 20150124.1156 from the zop-to-char source

--- zop-to-char.el
+++ zop-to-char.el
@@ -1,5 +1,5 @@
-;;; zop-to-char.el --- A replacement of zap-to-char.
-;; Version: 20140416.1015
+;;; zop-to-char.el --- A replacement of zap-to-char. -*- lexical-binding: t -*-
+;; Version: 20150124.1156
 
 ;; Author: Thierry Volpiatto <thierry.volpiatto@gmail.com>
 ;; Copyright (C) 2010~2014 Thierry Volpiatto, all rights reserved.
@@ -37,6 +37,16 @@
 
 ;; Internal
 (defvar zop-to-char--delete-up-to-char nil)
+(defvar zop-to-char--last-input nil)
+(defun zop-to-char-info-in-mode-line (prompt doc)
+  "Display string STR in mode-line."
+  (with-current-buffer
+      (window-buffer (with-selected-window (minibuffer-window)
+                       (minibuffer-selected-window)))
+    (let ((mode-line-format (concat " " (concat prompt zop-to-char--last-input doc))))
+      (force-mode-line-update)
+      (sit-for 12))
+    (force-mode-line-update)))
 
 ;;;###autoload
 (defun zop-to-char (arg)
@@ -45,13 +55,22 @@
   (let* ((pos    (point))
          (ov     (make-overlay pos (1+ pos)))
          (char   "")
+         timer
          (prompt (propertize "Zap to char: " 'face 'minibuffer-prompt))
          (doc    (propertize
                   "   [RET/C-k:kill, C-c/M-w:copy, C-f/C-n/right:next, C-b/C-p/left:prec, C-g:abort, C-q:quit, DEL:erase input]"
                   'face 'minibuffer-prompt)))
     (overlay-put ov 'face 'region)
+    (and (eobp) (setq arg -1))
+    (setq zop-to-char--last-input char)
+    (when (minibufferp (current-buffer))
+      (setq timer (run-with-idle-timer
+                   0.1 t
+                   'zop-to-char-info-in-mode-line
+                   prompt doc)))
     (unwind-protect
-        (while (let ((input (read-key (concat prompt char doc)))
+        (while (let ((input (read-key (unless (minibufferp (current-buffer))
+                                        (concat prompt char doc))))
                      (beg   (overlay-start ov))
                      (end   (overlay-end ov)))
                  (cl-case input
@@ -64,27 +83,39 @@
                      beg (if zop-to-char--delete-up-to-char
                              (1- end) end))
                     (goto-char pos) nil)
-                   ((right ?\C-f ?\C-n) ; Next occurence.
+                   ((right ?\C-f ?\C-n) ; Next occurrence.
                     (setq arg 1) t)
-                   ((left ?\C-b ?\C-p)  ; Prec occurence.
+                   ((left ?\C-b ?\C-p)  ; Prec occurrence.
                     (setq arg -1) t)
                    ((?\d ?\C-d)   ; Erase input.
-                    (setq char "") (goto-char pos) t)
+                    (setq char "") (goto-char pos)
+                    (setq zop-to-char--last-input char)
+                    (delete-overlay ov)
+                    t)
                    (?\C-q nil)    ; Quit at point
                    ((?\C-g ?\e)   ; Quit at pos.
                     (goto-char pos) nil)
                    (t             ; Input string.
                     (when (characterp input)
-                      (setq char (string input))))))
-          (if (< arg 0)
-              (search-backward char nil t (- arg))
-            (forward-char 1)
-            (search-forward char nil t arg)
-            (forward-char -1))
-          (if (< (point) pos)
-              (move-overlay ov (1+ pos) (point))
-            (move-overlay ov pos (1+ (point)))))
+                      (setq char (string input))
+                      (setq zop-to-char--last-input char)))))
+          (condition-case _err
+              (progn
+                (if (< arg 0)
+                    (search-backward char nil t (- arg))
+                  (forward-char 1)
+                  (search-forward char nil t arg)
+                  (forward-char -1))
+                (if (<= (point) pos)
+                    (move-overlay ov (1+ pos) (point))
+                  (move-overlay ov pos (1+ (point)))))
+            (scan-error nil)
+            (end-of-buffer nil)
+            (beginning-of-buffer nil)))
       (message nil)
+      (when timer
+        (cancel-timer timer) (setq timer nil))
+      (force-mode-line-update)
       (delete-overlay ov))))
 
 ;;;###autoload
